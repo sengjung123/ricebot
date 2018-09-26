1825696fa434343c1b5207297be2229d6cdaee0d
Glitch (ricebot) <none>
2018-09-26 14:20:15 +0000

    v0.4.3a: quick fix, added 1 new command for rude people

--- index.js
+++ index.js
@@ -22,39 +22,6 @@ const PACKAGE = require('./package.json');
 
 var fs = require('fs');
 
-var router = express.Router();
-var content = fs.readFileSync('.glitch-assets', 'utf8');
-var rows = content.split("\n");
-var assets = rows.map((row) => {
-  try {
-    return JSON.parse(row);
-  } catch (e) {}
-});
-assets = assets.filter((asset) => asset);
-
-// Example url
-// https://cdn.gomix.com/us-east-1%3A1a0f89c8-26bf-4073-baed-2b409695e959%2Ffoobar.png
-
-router.use((request, response) => {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Methods", "GET");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  
-  var path = request.path.substring(1);
-  
-  var [matching] = assets.filter((asset) => {
-    if(asset.name)
-      return asset.name.replace(/ /g,'%20') === path;
-  });
-  
-  if (!matching || !matching.url) {
-    return response.status(404).end("No such file");
-  }
-  
-  return response.redirect(matching.url);
-});
-
-module.exports = router;
 
 var isReady = true;
 
@@ -279,6 +246,33 @@ client.on("message", (message) => {
             {  
               var playSound = connection;
               var dispatcher = playSound.playFile('./audio/hiimrice.mp3');
+              dispatcher.on("end", end => {
+                try {voiceChannel.leave();; }catch(e){ console.log(e); }
+                voiceChannel = undefined;
+            });
+          });
+        }catch(e)
+        {
+          console.log(e);
+          voiceChannel = undefined;
+          isReady = true;
+      }
+      isReady = true;
+      }
+    break;
+  case "suckdick" :
+    if (isReady)
+      {
+         isReady = false;
+         var voiceChannel = message.member.voiceChannel;
+         //if (!voiceChannel)
+         //   isReady = true;
+         //   return message.reply("You must be in a voice channel to use this command.");
+         try{
+            voiceChannel.join().then(connection =>
+            {  
+              var playSound = connection;
+              var dispatcher = playSound.playFile('./audio/suckadick.mp3');
               dispatcher.on("end", end => {
                 try {voiceChannel.leave();; }catch(e){ console.log(e); }
                 voiceChannel = undefined;
